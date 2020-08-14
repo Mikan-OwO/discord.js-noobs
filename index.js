5d44a63f952d736520dc4199b98e7fe3f80cb0a8
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-14 14:06:11 +0900

    こみってど
    こみってど

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const discord = require("discord.js");
 const client = new discord.Client();
 const fs = require("fs");
 
-async function (message, [command, ...args]) {
-}
-
 exports.login = function (token) {
   return client.login(token);
 }
