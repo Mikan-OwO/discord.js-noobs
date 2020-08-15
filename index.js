7ac469d2685f8ae536f6d250d09ba1b1d54c87d7
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-15 09:26:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const discord = require("discord.js");
 const client = new discord.Client();
 const fs = require("fs");
 
+let prefix;
+
 exports.prefix = function prefix(index) {
   return prefix = index;
 }
 
-exports.command = async function run(cmd, send) {
-  client.on("message", message => {
+exports.command = async function Run(message) {
     if(!message.content.startsWith(prefix)) return;
     const [command, ...args] = message.content.slice(prefix.length).split(" ");
-    if(command === cmd) {
-      message.channel.send(send);
-    }
-  });
+  if(command === "") message.channel.send();
 }
+
+client.on("message", message => Run(message));
 
 exports.login = function (token) {
   return client.login(token);
