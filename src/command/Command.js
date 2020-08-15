5d9e671614eefb0bcdbdb20d97aa056f202ff4dc
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-15 16:41:31 +0900

    Update Command.js

--- src/command/Command.js
+++ src/command/Command.js
@@ -1,1 +1,8 @@
+const discord = require("discord.js");
+const client = new discord.Client();
 
+function Run(message) {
+  const [command, ...arga] = message.content.slice(prefix.length).split(" ");
+}
+
+client.on("message", message => Run(message));
