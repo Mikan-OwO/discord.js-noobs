c49209a05ba2d870a8c02e088ad704284a4308f6
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-15 14:53:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,4 +18,5 @@ client.on("message", message => Run(message));
 
 exports.login = function (token) {
   return client.login(token);
+  client.once("ready", () => console.log(client.user.tag + "でログインしました。"));
 }
