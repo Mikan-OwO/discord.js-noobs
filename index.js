acc48532379ac8a6f3d0672270f17b18e699cdbf
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-15 15:29:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const discord = require("discord.js");
 const client = new discord.Client();
 const fs = require("fs");
 
-function Run(message) {
-  if(message.content === "!avatar") message.channel.send(message.author.avatarURL());
-}
-
-client.on("message", message => Run(message));
-
-exports.login = function (token) {
-  return client.login(token);
-  client.once("ready", () => console.log(client.user.tag + "でログインしました。"));
+module.exports => {
+  Client: require("./src/client/client");
 }
