c0a8665156d437d711df400c33cdd0a8da08e556
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-17 14:07:45 +0900

    Update Client.js

--- src/client/Client.js
+++ src/client/Client.js
@@ -3,6 +3,14 @@
 const discord = require("discord.js");
 const client = new discord.Client();
 
+exports.message = function (name, value) {
+  function Run(message) {
+    if(message.content === name) message.channel.send(value);
+  }
+}
+
+client.on("message", message => Run(message));
+
 exports.login = function (token) {
   return client.login(token);
 }
