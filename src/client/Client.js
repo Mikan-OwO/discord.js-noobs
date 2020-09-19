5571cc2d26ebc0204ad1ab84ab13e02b91773631
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-09-19 12:40:12 +0900

    Update Client.js

--- src/client/Client.js
+++ src/client/Client.js
@@ -1,20 +1,7 @@
-const discord = require("discord.js");
-const client = new discord.Client();
+const DiscordClient = require("discord.js").Client;
 
-exports.message = function (name, value) {
-  client.on("message", message => {
-    if(message.content === name) message.channel.send(value);
-  })
+class Client extends DiscordClient {
+constructor(options = {}) {
+super(options);
 }
-
-exports.command = function (prefix, name, value) {
-  client.on("message", message => {
-    if(!message.content.startsWith(prefix)) return;
-    const [command, ...args] = message.content.slice(prefix.length).split(" ");
-    if(command === name) message.channel.send(value);
-  })
-}
-
-exports.login = function (token) {
-  return client.login(token);
 }
