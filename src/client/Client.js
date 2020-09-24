6cb3051de0f3436e0dd0017081c95a2ab184e9f4
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-09-24 19:53:32 +0900

    Update Client.js

--- src/client/Client.js
+++ src/client/Client.js
@@ -2,15 +2,7 @@ const DiscordClient = require("discord.js").Client;
 
 class Client extends DiscordClient {
   constructor(prefix, options = {}) {
-    this.prefix = prefix;
-    this.commands = [];
     super(options);
-  }
-  addCommand(options = {}) {
-    this.commands.push({
-      name: options.name,
-      scripts: options.run
-    });
   }
 }
 
