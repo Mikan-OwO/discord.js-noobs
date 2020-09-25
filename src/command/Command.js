cace5e3d684490f56df39ae26c71b237a8b7c308
Mikan-OwO <hinata721mikan@gmail.com>
2020-09-25 19:05:01 +0900

    update

--- src/command/Command.js
+++ src/command/Command.js
@@ -1,13 +1,13 @@
 class Command {
     constructor(options = {}) {
-        this.prefix = String(options.token);
+        this.prefix = String(options.prefix);
         this.message = options.message;
     }
     addCommand(options = {}) {
         const [command,...args] = this.message.content.slice(this.prefix.length).split(" ");
-        this.#name = String(options.name);
-        this.#script = options.run;
-        if(command === this.#name) this.#script;
+        this.name = String(options.name);
+        this.script = options.run;
+        if(command === this.name) this.script;
 
         return this;
     }
