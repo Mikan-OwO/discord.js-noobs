02ea25fc822d1f1b7ee67777561a54fe89bff091
Mikan-OwO <hinata721mikan@gmail.com>
2021-02-07 12:08:37 +0900

    fix

--- src/test/test.js
+++ src/test/test.js
@@ -1,9 +1,6 @@
 const { Client } = require("../index");
 
-const client = new Client(
-  "NzI5OTg2ODkyNjc3OTA2NDMy.XwQ7RQ.BUIAyOpCSF-tFzrhMZcrJcpsxN4",
-  "m!"
-);
+const client = new Client("your token", "prefix");
 
 client
   .command("test", (msg, args) => {
